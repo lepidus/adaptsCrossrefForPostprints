0ade772a4f8c30aa16d9e944991c3d029545a6cc
Jhon <jhon@lepidus.com.br>
2024-11-22 14:45:03 -0400

    Adds unit test - Adding of translation info node
    Issue: documentacao-e-tarefas/scielo#720
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- tests/CrossrefExportAdapterTest.php
+++ tests/CrossrefExportAdapterTest.php
@@ -1,18 +1,20 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use APP\submission\Submission;
 use APP\plugins\generic\crossref\CrossrefExportDeployment;
 use APP\plugins\generic\adaptsCrossrefForPostprints\classes\CrossrefExportAdapter;
 
 class CrossrefExportAdapterTest extends TestCase
 {
     private $crossrefXml;
-    private $crossrefExportAdapter;
+    private $submission;
+    private $adaptedExport;
 
     public function setUp(): void
     {
         $this->crossrefXml = $this->loadCrossrefXml();
-        $this->crossrefExportAdapter = new CrossrefExportAdapter();
+        $this->submission = $this->createSubmission();
     }
 
     private function loadCrossrefXml()
@@ -24,10 +26,19 @@ class CrossrefExportAdapterTest extends TestCase
         return $xml;
     }
 
+    private function createSubmission(): Submission
+    {
+        $submission = new Submission();
+        $submission->setData('id', 27);
+        $submission->setData('isTranslationOfDoi', '10.7531/OriginalArticle101');
+        $submission->setData('locale', 'pt_BR');
+
+        return $submission;
+    }
+
     public function testAdaptationChangesContentType(): void
     {
-        $adaptedExport = $this->crossrefExportAdapter->adaptExport($this->crossrefXml);
-        $submissionNodes = $adaptedExport->getElementsByTagName('posted_content');
+        $submissionNodes = $this->adaptedExport->getElementsByTagName('posted_content');
 
         foreach ($submissionNodes as $submissionNode) {
             $this->assertEquals('other', $submissionNode->getAttribute('type'));
@@ -43,13 +54,38 @@ class CrossrefExportAdapterTest extends TestCase
         );
         $this->assertEquals(1, $originalRelationsNode->count());
 
-        $adaptedExport = $this->crossrefExportAdapter->adaptExport($this->crossrefXml);
-        $adaptedSubmissionNode = $adaptedExport->getElementsByTagName('posted_content')->item(0);
+        $adaptedSubmissionNode = $this->adaptedExport->getElementsByTagName('posted_content')->item(0);
         $adaptedRelationsNode = $adaptedSubmissionNode->getElementsByTagNameNS(
             CrossrefExportDeployment::CROSSREF_XMLNS_REL,
             'program'
         );
 
         $this->assertEquals(0, $adaptedRelationsNode->count());
+    }
+
+    public function testAddsTranslationInformationNode(): void
+    {
+        $submissionNode = $this->adaptedExport->getElementsByTagName('posted_content')->item(0);
+        $programNode = $submissionNode->getElementsByTagNameNS(
+            CrossrefExportDeployment::CROSSREF_XMLNS_REL,
+            'program'
+        )->item(0);
+        $relatedItemNode = $programNode->getElementsByTagName(
+            CrossrefExportDeployment::CROSSREF_XMLNS_REL,
+            'related_item'
+        )->item(0);
+        $descriptionNode = $relatedItemNode->getElementsByTagName(
+            CrossrefExportDeployment::CROSSREF_XMLNS_REL,
+            'description'
+        )->item(0);
+        $intraWorkRelationNode = $relatedItemNode->getElementsByTagName(
+            CrossrefExportDeployment::CROSSREF_XMLNS_REL,
+            'intra_work_relation'
+        )->item(0);
+
+        $this->assertEquals('Portuguese translation', $descriptionNode->nodeValue);
+        $this->assertEquals('isTranslationOf', $intraWorkRelationNode->getAttribute('relationship-type'));
+        $this->assertEquals('doi', $intraWorkRelationNode->getAttribute('identifier-type'));
+        $this->assertEquals($this->submission->getData('isTranslationOfDoi'), $intraWorkRelationNode->nodeValue);
     }
 }
